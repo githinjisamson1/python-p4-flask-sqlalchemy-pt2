4c2f4a588796c556bfcf2d69124db6394a4c2048
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-09 14:43:17 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,17 +1,71 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Pet, Owner
 
 app = Flask(__name__)
+# point to existing db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# set to False to avoid building up too much unhelpful data in memory when our application is running.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# setup for migrations
 migrate = Migrate(app, db)
 
+# connect db to app before running
 db.init_app(app)
+
+
+# routes and views
+@app.route("/")
+def index():
+    return make_response('<h1>Welcome to the pet/owner directory!</h1>',
+                         200)
+
+
+# route parameter
+@app.route("/pets/<int:id>")
+def pet_by_id(id):
+    # pet = Pet.query.filter(Pet.id == id).first()
+    pet = Pet.query.filter_by(id=id).first()
+
+    if not pet:
+        response_body = "<h1>404 pet not found</h1>"
+        return make_response(response_body, 404)
+
+    response_body = f'''
+        <h1>Information for {pet.name}</h1>
+        <h2>Pet Species is {pet.species}</h2>
+        <h2>Pet Owner is {pet.owner.name}</h2>
+    '''
+
+    return make_response(response_body, 200)
+
+
+@app.route("/owners/<int:id>")
+def owner_by_id(id):
+    # owner = Owner.query.filter(Owner.id == id).first()
+    owner = Owner.query.filter_by(id=id).first()
+
+    if not owner:
+        return make_response("<h1>404 owner not found!</h1>", 404)
+
+    response_body = f'''
+    <h1>Information for {owner.name}</h1>
+    '''
+    # 1:M relationship
+    pets = [pet for pet in owner.pets]
+
+    if not pets:
+        response_body += f"<h2>Has no pets at this time</h2>"
+    else:
+        for pet in pets:
+            response_body += f"<h2>Has pet {pet.species} named {pet.name}.</h2>"
+
+    return make_response(response_body, 200)
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -6,3 +6,52 @@ from faker import Faker
 
 from app import app
 from models import db, Owner, Pet
+
+# db is generated with each new run of the application- it lives in models.py/connect db and application
+# db.init_app(app)
+
+fake = Faker()
+
+# ensures that applications fail quickly if they are not configured with this important context.
+with app.app_context():
+    # empty tables before seeding
+    Pet.query.delete()
+    Owner.query.delete()
+
+    # track owner objects
+    owners = []
+
+    # generate 50 objects
+    for item in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
+
+    # insert to db
+    db.session.add_all(owners)
+
+    # effect changes
+    db.session.commit()
+
+    pets = []
+    for item in range(100):
+        species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+        pet = Pet(name=fake.first_name(), species=rc(
+            species), owner=rc(owners))
+        pets.append(pet)
+
+    db.session.add_all(pets)
+    db.session.commit()
+
+
+# QUERIES
+# Queries in Flask-SQLAlchemy have access to all of the same methods as those in vanilla SQLAlchemy- just remember to start your statements with models!
+
+# // imports
+# >> with app.app_context():
+# = 'Ben').all()
+#  => [<Pet Owner Brenda Hernandez>, <Pet Owner Brian Stone>, ...]
+
+# >> with app.app_context():
+# ...     Owner.query.filter(Owner.name <= 'Ben').limit(2).all()
+# ...
+#  => [<Pet Owner Alan Bryant>, <Pet Owner Allison Phillips DDS>]
